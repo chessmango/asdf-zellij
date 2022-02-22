c6cb8f34742c77e8066016a6476630016ecb63f4
Owen Valentine <owenvalentine@gmail.com>
2022-02-22 13:43:56 +0200

    Add chmod

--- lib/utils.bash
+++ lib/utils.bash
@@ -64,6 +64,7 @@ install_version() {
     # TODO: Asert awsls executable exists.
     local tool_cmd
     tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
+    chmod +x "$install_path/bin/$tool_cmd"
     test -x "$install_path/bin/$tool_cmd" || fail "Expected $install_path/bin/$tool_cmd to be executable."
 
     echo "$TOOL_NAME $version installation was successful!"
