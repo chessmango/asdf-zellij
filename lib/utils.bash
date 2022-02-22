2933376b6a16965326404bab1461da212585c7e0
Owen Valentine <owenvalentine@gmail.com>
2022-02-22 16:12:31 +0200

    Remove echo

--- lib/utils.bash
+++ lib/utils.bash
@@ -46,7 +46,6 @@ download_release() {
 
   echo "* Downloading $TOOL_NAME release $version..."
   curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
-  echo curl "${curl_opts[@]}" -o "$filename" -C - "$url"
 }
 
 install_version() {
@@ -70,7 +69,7 @@ install_version() {
 
     echo "$TOOL_NAME $version installation was successful!"
   ) || (
-    echo rm -rf "$install_path"
+    rm -rf "$install_path"
     fail "An error ocurred while installing $TOOL_NAME $version."
   )
 }
